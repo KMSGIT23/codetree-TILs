0d5bdc15f4bd00cb7072a2cedb68c5aefa6d1bc4
김민성 <128460782+Kmin3@users.noreply.github.com>
2023-12-09 19:09:35 +0900

    Title: 배열의 평균, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231209/배열의 평균/ave-of-array.py
+++ 231209/배열의 평균/ave-of-array.py
@@ -6,4 +6,4 @@ print("%.1f %.1f "%(sum(a[0]) / 4, sum(a[1]) / 4))
 for i in range(4):
     print("%.1f "%((a[0][i] + a[1][i]) / 2), end = '')
 
-print("%.1f"%((sum(a[0]) + sum(a[1])) / 8))
+print("\n%.1f"%((sum(a[0]) + sum(a[1])) / 8))
