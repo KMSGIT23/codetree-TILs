ac70a8b0c49f24c748211bb83f4b919a0110bf9c
김민성 <128460782+Kmin3@users.noreply.github.com>
2023-12-09 19:09:09 +0900

    Title: 배열의 평균, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231209/배열의 평균/ave-of-array.py
+++ 231209/배열의 평균/ave-of-array.py
@@ -2,8 +2,8 @@ a = []
 for i in range(2):
     p = list(map(int, input().split()))
     a.append(p)
-print(sum(a[0]) / 4, sum(a[1]) / 4, '')
+print("%.1f %.1f "%(sum(a[0]) / 4, sum(a[1]) / 4))
 for i in range(4):
-    print((a[0][i] + a[1][i]) / 2, end = ' ')
+    print("%.1f "%((a[0][i] + a[1][i]) / 2), end = '')
 
-print((sum(a[0]) + sum(a[1])) / 8)
+print("%.1f"%((sum(a[0]) + sum(a[1])) / 8))
