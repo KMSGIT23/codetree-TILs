19266f02a54d82d2c705a38628a1f6d4929635aa
김민성 <128460782+Kmin3@users.noreply.github.com>
2023-12-11 00:23:00 +0900

    Title: 공약수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231211/공약수/common-divisor.py
@@ -0,0 +1,11 @@
+def f(a, b):
+    if b==0:
+        return a
+    return f(b, a%b)
+if __name__=="__main__":
+    n = int(input())
+    a, b=map(int, input().split())
+    c = f(a, b)
+    for i in range(1, c + 1):
+        if c % i == 0:
+            print(i)
