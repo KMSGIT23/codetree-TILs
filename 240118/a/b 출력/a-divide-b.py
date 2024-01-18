32021323618676d6eeb21688288406a0c6c53596
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-18 15:12:35 +0900

    Title: a/b 출력, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/a/b 출력/a-divide-b.py
+++ 240118/a/b 출력/a-divide-b.py
@@ -1,20 +1,8 @@
-'''
 a, b = map(int, input().split())
 c = round(a / b, 1)
 print(c, end = '')
-a = a * 10 % b
+a = a % b * 10 % b
 for _ in range(19):
     a *= 10
     print(a//b, end='')
-    a %= b
-'''
-a, b= map(int, input().split())
-
-print(f'{a//b}.', end='')
-
-a %= b
-for _ in range(20):
-    a *= 10
-    print(a//b, end='')
-
     a %= b
