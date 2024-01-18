d80c5c8c9f50f7e87f051a454bdaf6a535664182
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-18 15:11:37 +0900

    Title: a/b 출력, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/a/b 출력/a-divide-b.py
+++ 240118/a/b 출력/a-divide-b.py
@@ -1,3 +1,4 @@
+'''
 a, b = map(int, input().split())
 c = round(a / b, 1)
 print(c, end = '')
@@ -5,4 +6,16 @@ a = a * 10 % b
 for _ in range(19):
     a *= 10
     print(a//b, end='')
+    a %= b
+'''
+arr=input().split()
+a, b= int(arr[0]), int(arr[1])
+
+print(f'{a//b}.', end='')
+
+a %= b
+for _ in range(20):
+    a *= 10
+    print(a//b, end='')
+
     a %= b
