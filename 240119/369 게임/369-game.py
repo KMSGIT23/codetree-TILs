d790f9b564ebb146662c07e2376819d4c8e29727
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-19 15:11:33 +0900

    Title: 369 게임, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/369 게임/369-game.py
+++ 240119/369 게임/369-game.py
@@ -1,6 +1,6 @@
 n = int(input())
 for i in range(1, n + 1):
-    if i % 3 == 0 or list(str(i)) in '3' or list(str(i)) in '6' or list(str(i)) in '9':
+    if i % 3 == 0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
         print(0, end = ' ')
     else:
         print(i, end = ' ')
