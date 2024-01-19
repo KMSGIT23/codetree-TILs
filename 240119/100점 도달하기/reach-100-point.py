7e60ed9f82dee2cfaf4dea7d30766126ba8772e7
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-19 13:38:44 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240119/100점 도달하기/reach-100-point.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(n, 101):
+    if i >= 90:
+        print('A', end == ' ')
+    if i >= 80:
+        print('B', end == ' ')
+    if i >= 70:
+        print('C', end == ' ')
+    if i >= 60:
+        print('D', end == ' ')
+    else:
+        print('F', end = ' ')
