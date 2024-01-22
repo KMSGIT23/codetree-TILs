bf99e75144c2cd1842fc52480a407fcb3538723e
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-22 09:25:03 +0900

    Title: 0 나올때까지 출력, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/0 나올때까지 출력/print-until-zero-is-given.py
+++ 240122/0 나올때까지 출력/print-until-zero-is-given.py
@@ -1,8 +1,14 @@
 last = []
+
 while True:
-    n = int(input())
-    if n != 0:
-        last.append(n)
-    else:
-        for i in last:
-            print(i)
+    try:
+        n = int(input())
+        if n != 0:
+            last.append(n)
+        else:
+            break  # Exit the loop when n is 0
+    except EOFError:
+        break  # Exit the loop if EOFError occurs (end of input)
+
+for i in last:
+    print(i)
