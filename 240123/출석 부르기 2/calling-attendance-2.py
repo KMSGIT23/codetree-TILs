b16e487970b6e578850d09be190f82388b31d8b2
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-23 01:27:13 +0900

    Title: 출석 부르기 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240123/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        n = int(input())
+        if n == 1:
+            print('John')
+        elif n == 2:
+            print('Tom')
+        elif n == 3:
+            print('Paul')
+        elif n == 4:
+            print('Sam')
+        else:
+            print('Vacancy')
+            break
+    except:
+        break
