3e8af1576bbc649cfcd4f92ae2ec174b56c77cb1
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-01-28 10:35:08 +0900

    Title: 체크 다이아몬드, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240128/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,11 @@
+n = int(input())
+j = n - 1
+for i in range(1, n):
+    print(' ' * j, end = '')
+    print('* ' * i)
+    j -= 1
+j = 0
+for i in range(n, 0, -1):
+    print(' ' * j, end = '')
+    print('* ' * i)
+    j += 1
