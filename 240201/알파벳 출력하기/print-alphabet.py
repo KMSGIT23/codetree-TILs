266a7966227ab0bc0314383ca3d6428d5463d80f
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-02-01 01:19:24 +0900

    Title: 알파벳 출력하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/알파벳 출력하기/print-alphabet.py
+++ 240201/알파벳 출력하기/print-alphabet.py
@@ -3,5 +3,8 @@ t = 65
 for i in range(n):
     for j in range(i+1):
         print(chr(t), end = '')
-        t += 1
+        if t == 90:
+            t = 65
+        else:
+            t += 1
     print()
