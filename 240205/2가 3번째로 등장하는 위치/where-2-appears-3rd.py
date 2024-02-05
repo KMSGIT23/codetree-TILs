8aea3c5f828bcd91f0b384fb666d4a7b39d18d64
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-02-05 15:26:08 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 251ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240205/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -3,7 +3,10 @@ a = list(map(int, input().split()))
 cnt = 0
 for i in range(n):
     if cnt == 3:
-        print(i)
+        print(i+1)
         break
     if a[i] == 2:
         cnt += 1
+        if cnt == 3:
+            print(i+1)
+            break
