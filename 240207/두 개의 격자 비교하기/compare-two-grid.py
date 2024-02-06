618a5fe2d2be46e3a03f7d23988d4eaebd7bfd28
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-02-07 00:14:40 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240207/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+a = []
+b = []
+c = [[0]*m for i in range(m)]
+for i in range(n):
+    p = list(map(int, input().split()))
+    a.append(p)
+for i in range(n):
+    q = list(map(int, input().split()))
+    b.append(q)
+for i in range(n):
+    for j in range(m):
+        if a[i][j] != b[i][j]:
+            c[i][j] = 1
+for i in range(n):
+    print(*c[i])
