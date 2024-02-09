7705b3719aa043effc183a9018254015ff43f1cb
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-02-09 12:42:21 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240209/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
+++ 240209/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
@@ -1,8 +1,14 @@
 w = input()
 a = list(w)
 a.reverse()
-b = []
-for i in range(len(w)):
-    if i % 2 == 1:
-        b.append(a[i])
-print(*b)
+if len(w) % 2 == 0:
+    for i in range(len(w)):
+        if i % 2 == 0:
+            print(a[i], end = '')
+else:
+    b = []
+    for i in range(len(w)):
+        if i % 2 == 1:
+            b.append(a[i])
+    b = ''.join(b)
+    print(b)
