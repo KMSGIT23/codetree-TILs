10631864a51ff196830ac76e745dadba51a3dd47
김민성 <128460782+Kmin3@users.noreply.github.com>
2024-02-13 12:00:19 +0900

    Title: 대문자로 출력하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/대문자로 출력하기/print-in-capital.py
+++ 240213/대문자로 출력하기/print-in-capital.py
@@ -1,6 +1,6 @@
 x = input()
 s = []
 for i in range(len(x)):
-    if ord(x[i]) >= 65 and ord(x[i]) <= 122:
+    if 90 >= ord(x[i]) >= 65 or 97 <= ord(x[i]) <= 122:
         s.append(x[i].upper())
 print(''.join(s))
