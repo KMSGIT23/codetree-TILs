bb0bd61a0b8462f9ae44b940480ebd3f9044eaa4
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-21 00:59:51 +0900

    Title: 100을 넘기는 순간, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/100을 넘기는 순간/as-soon-as-hit-100.py
+++ 240221/100을 넘기는 순간/as-soon-as-hit-100.py
@@ -1,9 +1,4 @@
 n = int(input())
 a = list(map(int, input().split()))
-b = []
-for i in a:
-    b.append(i)
-    if i >= 100:
-        break
-print(sum(b))
-print("%.1f"%(sum(b) / len(b)))
+print(sum(a))
+print("%.1f"%(sum(a) / len(a)))
