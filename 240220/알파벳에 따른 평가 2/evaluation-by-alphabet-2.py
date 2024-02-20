447431edecf5cc566414975bd205d8c13a3c2447
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-20 13:14:53 +0900

    Title: 알파벳에 따른 평가 2, Time: 160ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240220/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,13 @@
+n = input()
+if n == 'S':
+    print('Superior')
+elif n == 'F':
+    print('Fantastic')
+elif n == 'G':
+    print('Good')
+elif n == 'U':
+    print('Usually')
+elif n == 'E':
+    print('Effort')
+else:
+    print('Failure')
