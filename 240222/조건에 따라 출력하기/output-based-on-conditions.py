396dfe99e46693db28bb5ce711bccbb6d0bfdb35
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-22 01:06:36 +0900

    Title: 조건에 따라 출력하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240222/조건에 따라 출력하기/output-based-on-conditions.py
+++ 240222/조건에 따라 출력하기/output-based-on-conditions.py
@@ -1,8 +1,10 @@
-while True:
+a = int(input())
+for i in range(a):
     n = int(input())
     if n == 0:
         break
-    if n % 3 == 0:
-        print(n // 3)
     else:
-        print(n + 3)
+        if n % 3 == 0:
+            print(n // 3)
+        else:
+            print(n + 2)
