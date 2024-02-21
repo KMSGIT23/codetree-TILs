3dc430732f62fec365bb7e16a4437d93522828ca
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-22 00:55:37 +0900

    Title: 5와 7의 배수를 제외하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240222/5와 7의 배수를 제외하기/excluding-multiples-of-5-and-7.py
+++ 240222/5와 7의 배수를 제외하기/excluding-multiples-of-5-and-7.py
@@ -1,7 +1,8 @@
 n = int(input())
-a = list(map(int, input().split()))
-for i in range(n):
-    if a[i] % 5 == 0 or a[i] % 7 == 0:
-        del a[i]
+arr = list(map(int, input().split()))
+a = []
+for i in a:
+    if i % 5 != 0 or i % 7 != 0:
+        a.append(i)
 print(sum(a))
 print("%.1f"%(sum(a) / len(a)))
