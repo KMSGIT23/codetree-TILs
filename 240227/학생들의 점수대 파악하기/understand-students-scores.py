531a4d032804db64394da6454272e0fb4f5aa7c2
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-27 00:12:47 +0900

    Title: 학생들의 점수대 파악하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/학생들의 점수대 파악하기/understand-students-scores.py
+++ 240227/학생들의 점수대 파악하기/understand-students-scores.py
@@ -4,6 +4,6 @@ s = [0 for i in range(10)]
 for i in a:
     if i >= 10:
         s[i // 10 - 1] += 1
-for i in range(n, 0, -1):
+for i in range(n-1, 0, -1):
     if s[i] != 0:
-        print(i, '-', s[i])
+        print(i+1, '-', s[i])
