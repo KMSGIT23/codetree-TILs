d198171395d73fa79f1b7d6010aadd260a1143a5
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-27 00:10:18 +0900

    Title: 학생들의 점수대 파악하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240227/학생들의 점수대 파악하기/understand-students-scores.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = list(map(int, input().split()))
+s = [0 for i in range(10)]
+for i in a:
+    if i >= 10:
+        s[i // 10 - 1] += 1
+for i in range(n, 0, -1):
+    if s[i] != 0:
+        print(i, '-', s[i])
