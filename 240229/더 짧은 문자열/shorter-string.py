437e1af39a192970866bd7a512d11a408aa7915f
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-29 00:39:34 +0900

    Title: 더 짧은 문자열, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240229/더 짧은 문자열/shorter-string.py
+++ 240229/더 짧은 문자열/shorter-string.py
@@ -2,4 +2,4 @@ a, b = input().split()
 if len(a) == len(b):
     print('equal')
 else:
-    print(a, len(a) if len(a) < len(b) else b, len(b))
+    print(a, len(a) if len(a) < len(b) else (b, len(b)))
