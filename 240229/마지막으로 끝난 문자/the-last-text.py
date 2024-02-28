54a5de471c23e0d34a28c05436c7b862818d7d0d
김민성 <128460782+Kmsu9@users.noreply.github.com>
2024-02-29 00:46:27 +0900

    Title: 마지막으로 끝난 문자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240229/마지막으로 끝난 문자/the-last-text.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = []
+s = []
+for i in range(n):
+    a.append(input())
+c = input()
+for i in a:
+    if i[-1] == c:
+        s.append(i)
+print(len(ss))
+for i in s:
+    print(i)
